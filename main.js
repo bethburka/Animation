c277f4b662ac9838aa32efafd9629b072726d8ef
Bethelehem <Beth@Bethelehem-MacBook-Pro.local>
2016-05-28 19:00:33 +0200

    Responsive banner Opacity

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 
-
 TweenLite.defaultEase = Power3.easeInOut;
 
-  //responsive timeline animation.
-  //values recorded once, nothing changes on resize
+//responsive timeline animation.
 var tl = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
-tl.from(".banner", 4, {xPercent:-100, force3D:true})
-  .to(".banner", 4, {xPercent:100}, "+=3");
-tl.play(10);
-
+tl.from(".banner", 4, {opacity: 0.3, force3D:true})
+//  .to(".banner", 1, {xPercent:100}, "+=3");
+tl.play(5);
 
 // Logo Animation
-TweenMax.from(".logo", 8, {
-  x:600,
+TweenMax.from(".logo", 4, {
+  x:200,
   width:200,
-  height:200,
-  padding:10,
-  backgroundColor:"#9a9a9a",
-  borderRadius:24});
+  borderRadius:14});
