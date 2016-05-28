4e694b477eef232b6f42ef3cb0639fbfe4e7d770
Bethelehem <Beth@Bethelehem-MacBook-Pro.local>
2016-05-28 23:48:30 +0200

    logo position fixed

--- main.js
+++ main.js
@@ -2,13 +2,11 @@
 TweenLite.defaultEase = Power3.easeInOut;
 
 //responsive timeline animation.
-var tl = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
-tl.from(".banner", 4, {opacity: 0.3, force3D:true})
+var strt = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
+strt.from(".banner", 4, {opacity: 0.3, force3D:true})
 //  .to(".banner", 1, {xPercent:100}, "+=3");
-tl.play(5);
+strt.play(5);
 
 // Logo Animation
-TweenMax.from(".logo", 4, {
-  x:200,
-  width:200,
-  borderRadius:14});
+TweenMax.from(".logo", 3, {
+  x:300});
