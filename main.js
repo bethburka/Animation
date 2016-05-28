2f3082d5b9257d12aa6cb2fb3d87229bfe687749
Bethelehem <Beth@Bethelehem-MacBook-Pro.local>
2016-05-28 15:54:47 +0200

    Banner

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-TweenMax.to(".logo", 4, {
+
+
+TweenLite.defaultEase = Power3.easeInOut;
+
+  //responsive timeline animation.
+  //values recorded once, nothing changes on resize
+var tl = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
+tl.from(".banner", 4, {xPercent:-100, force3D:true})
+  .to(".banner", 4, {xPercent:100}, "+=3");
+tl.play(10);
+
+
+// Logo Animation
+TweenMax.from(".logo", 8, {
   x:600,
-  padding:3,
+  width:200,
+  height:200,
+  padding:10,
   backgroundColor:"#9a9a9a",
   borderRadius:24});
