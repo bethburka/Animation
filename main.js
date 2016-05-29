00771702ae11817fb07ae045560ba3e7a21984c3
Bethelehem <Beth@Bethelehem-MacBook-Pro.local>
2016-05-29 23:57:06 +0200

    new banner colour and responsive

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ TweenLite.defaultEase = Power3.easeInOut;
 
 //responsive timeline animation.
 var strt = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
-strt.from(".banner", 4, {opacity: 0.3, force3D:true})
-//  .to(".banner", 1, {xPercent:100}, "+=3");
-strt.play(5);
+//.from(".banner", 4, {opacity: 0.5, force3D:false})
+  strt.to(".banner", 1, {xPercent:100}, "+=3");
+
 
 // Logo Animation
 TweenMax.from(".logo", 2.5, {
