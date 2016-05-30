9c8057e69e3fcfc4a8459a5fe624022d6468dda2
Bethelehem Burka <bethelehem.burka@mobgens-iMac-3.local>
2016-05-30 11:36:33 +0200

    Style modification

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ TweenLite.defaultEase = Power3.easeInOut;
 
 //responsive timeline animation.
 var strt = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
-//.from(".banner", 4, {opacity: 0.5, force3D:false})
-  strt.to(".banner", 1, {xPercent:100}, "+=3");
+  strt.from(".banner", 4, {opacity: 0.5, force3D:false})
+var strts = new TimelineMax({repeat:1, yoyo:true, repeatDelay:1})
+strts.to(".banner", 1, {xPercent:100}, "+=3");
 
 
 // Logo Animation
