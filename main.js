f8b005a1f0195a81a672201e03c7fc59b9a84fc2
Bethelehem <Beth@Bethelehem-MacBook-Pro.local>
2016-05-30 15:46:48 +0200

    commented some lines in man.js and text added in html file

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 TweenLite.defaultEase = Power3.easeInOut;
 
 //responsive timeline animation.
-var strt = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
-  strt.from(".banner", 4, {opacity: 0.5, force3D:false})
-var strts = new TimelineMax({repeat:1, yoyo:true, repeatDelay:1})
-strts.to(".banner", 1, {xPercent:100}, "+=3");
+var strt = new TimelineMax({repeat:1, yoyo:true, repeatDelay:1})
+  //strt.from(".banner", 4, {opacity: 0.5, force3D:false})
+//var strts = new TimelineMax({repeat:1, yoyo:true, repeatDelay:1})
+strt.to(".banner", 1, {xPercent:100}, "+=3");
 
+// var strt = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:1})
+//   strt.from(".banner", 4, {opacity: 0.5, force3D:false})
+// var strts = new TimelineMax({repeat:1, yoyo:true, repeatDelay:1})
+// strts.to(".banner", 1, {xPercent:100}, "+=3");
 
 // Logo Animation
-TweenMax.from(".logo", 2.5, {
-  ease:Expo.easeout,
-  x:320});
+// TweenMax.from(".logo", 2.5, {
+//   ease:Expo.easeout,
+//   x:320});
